fec1fc9222630877664b28d5cfb54115b8d34d2a
alexandra-tsidulko <alexandra.tsidulko@gmail.com>
2022-10-08 00:07:40 +0700

    выносит проверку аргументов в отдельную функцию

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
+// Проверка аргументов функций
+
+function checkRangeValues (value1, value2) {
+  if (value1 < 0 || value2 < 0 || typeof value1 !== 'number' || typeof value2 !== 'number') {
+    return true;
+  }
+}
+
 // Функция, возвращающая случайное целое число из переданного диапазона включительно
 
 function getRandomInteger (min, max) {
-  if(min < 0 || max < 0 || typeof min !== 'number' || typeof max !== 'number') {
-    return NaN;
-  }
-
-  return Math.floor(Math.random() * (Math.abs(Math.floor(max) - Math.ceil(min)) + 1) + Math.ceil(Math.min(min, max)));
+  return checkRangeValues(min, max) ? NaN : Math.floor(Math.random() * (Math.abs(Math.floor(max) - Math.ceil(min)) + 1) + Math.ceil(Math.min(min, max)));
 }
 
 getRandomInteger(1,2);
@@ -13,11 +17,7 @@ getRandomInteger(1,2);
 // Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
 
 function getRoundedRandomValue (min, max, decimal) {
-  if(min < 0 || max < 0 || typeof min !== 'number' || typeof max !== 'number') {
-    return NaN;
-  }
-
-  return Math.round((Math.random() * Math.abs(max - min) + Math.min(min, max)) * Math.pow(10, decimal)) / Math.pow(10, decimal);
+  return checkRangeValues(min, max) ? NaN : Math.round((Math.random() * Math.abs(max - min) + Math.min(min, max)) * Math.pow(10, decimal)) / Math.pow(10, decimal);
 }
 
 getRoundedRandomValue(0, 1, 5);
