b15dc33a2a620d24c764cf0dfb935200f659ae06
alexandra-tsidulko <alexandra.tsidulko@gmail.com>
2022-10-22 02:34:45 +0700

    деструктуризация

--- js/popup.js
+++ js/popup.js
@@ -14,24 +14,24 @@ const popupTypes = {
   hotel: 'Отель',
 };
 
-similarAds.forEach((popup) => {
+similarAds.forEach(({author, offer}) => {
   const newPopup = popupTemplate.cloneNode(true);
-  newPopup.querySelector('.popup__avatar').src = popup.author.avatar;
-  newPopup.querySelector('.popup__title').textContent = popup.offer.title;
-  newPopup.querySelector('.popup__text--address').textContent = popup.offer.address;
-  newPopup.querySelector('.popup__text--price').querySelector('[data-price]').textContent = popup.offer.price;
-  newPopup.querySelector('.popup__type').textContent = popupTypes[popup.offer.type];
-  newPopup.querySelector('.popup__text--capacity').textContent = `${popup.offer.rooms} комнаты для ${popup.offer.guests} гостей`;
-  newPopup.querySelector('.popup__text--time').textContent = `Заезд после ${popup.offer.checkin}, выезд до ${popup.offer.checkout}`;
+  newPopup.querySelector('.popup__avatar').src = author.avatar;
+  newPopup.querySelector('.popup__title').textContent = offer.title;
+  newPopup.querySelector('.popup__text--address').textContent = offer.address;
+  newPopup.querySelector('.popup__text--price').querySelector('[data-price]').textContent = offer.price;
+  newPopup.querySelector('.popup__type').textContent = popupTypes[offer.type];
+  newPopup.querySelector('.popup__text--capacity').textContent = `${offer.rooms} комнаты для ${offer.guests} гостей`;
+  newPopup.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
 
   const description = newPopup.querySelector('.popup__description');
-  if (popup.offer.description) {
-    description.textContent = popup.offer.description;
+  if (offer.description) {
+    description.textContent = offer.description;
   } else {
     description.remove();
   }
 
-  const popupFeatures = popup.offer.features;
+  const popupFeatures = offer.features;
   const featuresList = newPopup.querySelector('.popup__features').querySelectorAll('.popup__feature');
   const modifiers = popupFeatures.map((feature) => `popup__feature--${feature}`);
 
@@ -45,8 +45,8 @@ similarAds.forEach((popup) => {
   const photoBlock = newPopup.querySelector('.popup__photos');
   const photoTemplate = photoBlock.querySelector('.popup__photo').cloneNode(true);
   photoBlock.innerHTML = '';
-  if (popup.offer.photos) {
-    const photos = popup.offer.photos;
+  if (offer.photos) {
+    const photos = offer.photos;
     photos.forEach((photo) => {
       const newPhoto = photoTemplate.cloneNode(true);
       newPhoto.src = photo;
