e8a8ae966eafedfa8b158c675a53931e350d06ed
Keks <keks@htmlacademy.ru>
2022-11-05 13:42:56 +0300

    :heavy_check_mark: Сборка #9

--- 9/js/slider.js
+++ 9/js/slider.js
@@ -1,36 +1,7 @@
 const sliderElement = document.querySelector('.ad-form__slider');
 const priceField = document.querySelector('#price');
-const apartType = document.querySelector('#type');
-const minPrice = {
-  'palace': 10000,
-  'flat': 1000,
-  'house': 5000,
-  'bungalow': 0,
-  'hotel': 3000,
-};
 
-const changeSliderOptionsOnType = (slider, type, priceVariants) => {
-  type.addEventListener ('change', () => {
-    if (type.value === 'bungalow') {
-      slider.noUiSlider.updateOptions(
-        {range: {
-          min: 0,
-          max: 10000,
-        },
-        start: 0,
-        });
-    } else {
-      slider.noUiSlider.updateOptions(
-        {range: {
-          min: priceVariants[type.value],
-          max: 100000,
-        },
-        // start: priceVariants[type.value],
-        });
-    }});
-};
-
-const createSlider = (slider, price, type, priceVariants) => {
+const createSlider = (slider, price) => {
   noUiSlider.create(slider, {
     range: {
       min: 0,
@@ -56,8 +27,6 @@ const createSlider = (slider, price, type, priceVariants) => {
   price.addEventListener('change', () => {
     slider.noUiSlider.set(price.value);
   });
-
-  changeSliderOptionsOnType(slider, type, priceVariants);
 };
 
-createSlider(sliderElement, priceField, apartType, minPrice);
+createSlider(sliderElement, priceField);
