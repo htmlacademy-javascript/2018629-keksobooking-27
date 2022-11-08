fb1185d26d3aa505951a29afae289b29469f2335
Keks <keks@htmlacademy.ru>
2022-11-08 18:46:33 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/main.js
@@ -0,0 +1,24 @@
+import {createMap, createMarkers} from './map.js';
+import {turnFormOff, setFormSubmit } from './form.js';
+import {turnFiltersOff} from './filters.js';
+import { getData } from './api.js';
+import { showSuccessMessage } from './modal.js';
+
+const INIT_LOCATION = {
+  lat: 35.68211,
+  lng: 139.75364,
+};
+
+turnFiltersOff();
+turnFormOff();
+
+createMap(INIT_LOCATION);
+
+getData((otherAds) => {
+  const showedAds = otherAds.slice(0, 10);
+  showedAds.forEach((ad) => {
+    createMarkers(ad);
+  });
+});
+
+setFormSubmit(showSuccessMessage);
---
+++ 10/js/util.js
@@ -0,0 +1,66 @@
+// Функция из задания для получения случайного целого
+// Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
+const getRandomPositiveInteger = (a, b) => {
+  if (a < 0 || b < 0) {
+    return NaN;
+  }
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+// Функция из задания для получения случайного числа с плавающей точкой
+// Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
+const getRandomPositiveFloat = (a, b, digits = 1) => {
+  if (a < 0 || b < 0 || digits < 0) {
+    return NaN;
+  }
+  const lower = Math.min(a, b);
+  const upper = Math.max(a, b);
+  const result = Math.random() * (upper - lower) + lower;
+  return +result.toFixed(digits);
+};
+
+// Функция, возвращающая рандомный элемент из массива
+const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
+
+// Функция для перевода чисел вида 1 2 3 в вид 01 02 03;
+const getTwoDigitsNumber = (n) => (n < 10) ? `0${n}` : n;
+
+// Функция для получения массива случайной длины из значений, заданных в другом массиве. Значения могут повторяться.
+// Добавлена возможность выбирать максимальную длину нового массива
+const getRandomLengthArray = (data, maxLength) => {
+  const newArrayLength = getRandomPositiveInteger(1, maxLength);
+  const newArray = [];
+
+  for (let i = 0; i < newArrayLength; i++) {
+    const newElementIndex = getRandomPositiveInteger(0, (data.length - 1));
+    const newElement = data[newElementIndex];
+    newArray.push(newElement);
+  }
+
+  return newArray;
+};
+
+// Функция для получения массива случайной длины из неповторяющихся значений другого массива
+
+const getRandomLengthUniqueArray = (data) => {
+  const newArrayLength = getRandomPositiveInteger(1, data.length);
+  const newUniqueArray = [];
+
+  for (let i = 0; i < newArrayLength; i++) {
+    const newElementIndex = getRandomPositiveInteger(0, (data.length - 1));
+    const newElement = data[newElementIndex];
+    while (!newUniqueArray.includes(newElement)) {
+      newUniqueArray.push(newElement);
+    }
+  }
+
+  return newUniqueArray;
+};
+
+// Проверка кнопки
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+export {getRandomPositiveFloat, getRandomPositiveInteger, getRandomArrayElement, getRandomLengthUniqueArray, getRandomLengthArray, getTwoDigitsNumber, isEscapeKey};
